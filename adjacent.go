5b1c8442fab3dfeb98689f7972cd08d7c8b32a3c
telegrammae <telegrammae@gmail.com>
2019-10-07 20:37:15 -0400

    refactoring, better error handling, main file rename

--- adjacent.go
+++ adjacent.go
@@ -8,11 +8,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 )
 
 var (
-	group = flag.String("group", "s", "language group - choose from germanic (g), slavic (s), romance (r) - ")
+	group = flag.String("group", "", "language group - choose from germanic (g), slavic (s), romance (r)")
 	word  = flag.String("word", "language", "word to translate")
 )
 
@@ -23,9 +24,16 @@ type Response struct {
 }
 
 func main() {
+	flag.Usage = usage
 	flag.Parse()
 
-	languageList, err := checkLanguageGroup(*group)
+	var token = os.Getenv("YANDEX_TRANSLATE_TOKEN")
+	if token == "" {
+		log.Fatalf("your token for the API does not exist, set it as a value of the %s environment variable\n",
+			"YANDEX_TRANSLATE_TOKEN")
+	}
+
+	languageList, err := GetLanguageList(*group)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -35,7 +43,11 @@ func main() {
 		wg.Add(1)
 		l := languageList[i]
 		go func() {
-			request(*word, l)
+			translation, err := Request(*word, l, token)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(translation)
 			wg.Done()
 		}()
 	}
@@ -43,7 +55,17 @@ func main() {
 	wg.Wait()
 }
 
-func checkLanguageGroup(group string) ([]string, error) {
+func usage() {
+	fmt.Println("Usage:")
+	flag.PrintDefaults()
+}
+
+func GetLanguageList(group string) ([]string, error) {
+	if group == "" {
+		usage()
+		os.Exit(1)
+	}
+
 	var slavicList = []string{"ru", "be", "bg", "bs", "mk", "pl", "sr", "sk", "sl", "cs", "hr", "uk"}
 	var germanicList = []string{"af", "nl", "da", "is", "de", "no", "sv"}
 	var romanceList = []string{"it", "pt", "ro", "fr", "es", "ca"}
@@ -59,25 +81,25 @@ func checkLanguageGroup(group string) ([]string, error) {
 	}
 }
 
-//Make a request to the Yandex.Translate API to get the translation of a word from English to a given language.
-func request(word string, language string) {
-	const token = "[YOUR TOKEN]"
+// Request makes a request to the Yandex.Translate API to get the translation of a word
+// from English to a given language.
+func Request(word string, language string, token string) (string, error) {
 	url := fmt.Sprintf("https://translate.yandex.net/api/v1.5/tr.json/translate?key=%s&text=%s&lang=en-%s",
 		token, word, language)
 	response, err := http.Get(url)
 
 	if err != nil || response.StatusCode != http.StatusOK {
-		log.Fatalln("could not connect to the API, make sure your token is set and valid")
+		return "", errors.New("could not connect to the API, make sure your token is valid")
 	}
 	defer response.Body.Close()
 
 	bytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatalln("could not parse the response")
+		return "", err
 	}
 
 	var res Response
 	json.Unmarshal(bytes, &res)
 	output := fmt.Sprintf("%s\t%s", res.Lang, res.Text[0])
-	fmt.Println(output)
+	return output, nil
 }
